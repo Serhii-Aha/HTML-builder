4611e44d6bcb8edff8c519a7f0fb0acc28f62eb9
Serhii-Aha <aga31415926@gmail.com>
2021-11-08 18:29:49 +0200

    test ssh for new workstation

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -4,3 +4,19 @@ const components = path.join(__dirname, 'components');
 const projectDist = path.join(__dirname, 'project-dist');
 const assets = path.join(__dirname, 'assets');
 
+  function initAssets() {
+    fs.mkdir(path.join(__dirname, 'project-dist', 'assets'), { recursive: true }, (error) => {
+      if (error) return console.error(error.message);
+      copy();
+    });
+  }
+  function initHtml() {
+    fs.writeFile(path.join(__dirname, 'project-dist', 'index.html'), '', (error) => {
+      if (error) return console.error(error.message);
+    });
+  }
+  function initCss() {
+    fs.writeFile(path.join(__dirname, 'project-dist', 'style.css'), '', (error) => {
+      if (error) return console.error(error.message);
+    });
+  }
