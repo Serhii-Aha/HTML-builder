ee56816072fb1ebc4438c76f07fce71848f96c82
Serhii-Aha <aga31415926@gmail.com>
2021-11-08 18:05:54 +0200

    test new workstation

---
+++ 06-build-page/index.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+const components = path.join(__dirname, 'components');
+const projectDist = path.join(__dirname, 'project-dist');
+const assets = path.join(__dirname, 'assets');
+
